38bb65f98709425868429b4b11aef41110bd9c68
besher al addam <beshoux@outlook.fr>
2024-06-29 07:37:25 +0200

    Add more props to the stars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 /* import "./index.css";
 import App from "./App";
@@ -6,11 +6,27 @@ import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="green" onSetRating={setMovieRating} />
+      <p>{movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={128}
+      messages={["Terrible", "Bad", "OK", "Good", "Excellent"]}
+    />
+    <Test />
   </React.StrictMode>
 );
