3d36dcda7827a9c602036a7aa80ebf610b60e106
besher al addam <beshoux@outlook.fr>
2024-09-06 15:50:38 +0200

    add star rating

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,28 +1,5 @@
 import { useState, useEffect } from "react";
-
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+import StarRating from "./StarRating";
 
 const tempWatchedData = [
   {
@@ -249,7 +226,9 @@ function MovieDetails({ selectedId, onCloseMovie }) {
         <button className="btn-back" onClick={onCloseMovie}>
           &larr;
         </button>
+
         <img src={movie.Poster} alt={`Poster of the movie ${movie.Title}`} />
+
         <div className="details-overview">
           <h2>{movie.Title}</h2>
           <p>{movie.Released}</p>
@@ -260,13 +239,20 @@ function MovieDetails({ selectedId, onCloseMovie }) {
           </p>
         </div>
       </header>
+
       <section>
+        <div className="rating">
+          <StarRating maxRating={10} size={24} />
+        </div>
+
         <p>
           <em>{movie.Plot}</em>
         </p>
+
         <p>
           Starring <strong>{movie.Actors}</strong>
         </p>
+
         <p>
           Directed by <strong>{movie.Director}</strong>
         </p>
