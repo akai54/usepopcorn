b0f6d2cc2a7aa037c9004ed9293326976ce7d6e1
besher al addam <beshoux@outlook.fr>
2024-09-07 11:35:02 +0200

    adding abort controller

--- src/App-v1.js
+++ src/App-v1.js
@@ -32,12 +32,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setError("");
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error("Something went wrong");
@@ -46,11 +49,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("0 results found");
 
           setMovies(data.Search);
-          console.log(data.Search);
-          setIsLoading(false);
+          setError("");
         } catch (err) {
           console.error(err);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -62,6 +66,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
